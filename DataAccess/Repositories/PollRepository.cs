1eef8d6e4e77742c38ca6b4df405a62353526b37
Miguel Abela <abelamiguel@gmail.com>
2025-04-04 15:41:20 +0200

    Created the Poll Repository class

---
+++ DataAccess/Repositories/PollRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.DataContext;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class PollRepository
+    {
+        private PollDbContext myContext;
+
+        // Contructor Injection
+        public PollRepository(PollDbContext _myContext)
+        {
+            myContext = _myContext;
+        }
+
+        // This method will return all the list of Polls in the database
+        public IQueryable<Poll> GetPolls()
+        {
+            return myContext.Polls;
+        }
+
+        // This method will return a specific poll by the id
+        public Poll GetPoll(int pollId)
+        {
+            return myContext.Polls.SingleOrDefault(x => x.PollId == pollId)!;
+        }
+
+        // This method will enable the user to create a poll
+        public void CreatePoll(Poll poll)
+        {
+            myContext.Polls.Add(poll);
+            myContext.SaveChanges();
+        }
+    }
+}
